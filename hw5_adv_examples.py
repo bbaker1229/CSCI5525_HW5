28e5539d19fde2852a12b0d6150fa2242e44da88
Bryan John Baker <bake1358@umn.edu>
2020-04-21 23:49:49 -0500

    Initial file commit

---
+++ hw5_adv_examples.py
@@ -0,0 +1,17 @@
+############################################
+# Name: Bryan John Baker
+# Student ID: bake1358
+# Email: bake1358@umn.edu
+# Collaborators:
+# File Name: hw5_adv_examples.py
+############################################
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
